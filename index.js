1257a72acc6688a174127dbd5f80e3bc35bed4a0
German Cruz <cruzgerman216@yahoo.com>
2020-09-15 06:12:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 function writeCards(arr, element){
-  for(let i = 0; i<arr.length-1;i++){
+  for(let i = 0; i<arr.length;i++){
     console.log("Thank you, " + arr[i] + ", for the wonderful " + element + " gift!")
   }
 }
